9343307d1e217da1382ae6909e415890a9d636db
Rahid Haque <ontirahid80@gmail.com>
2026-03-24 09:05:21 +0600

    All basic setup done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold text-center'>Welcome to the Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
